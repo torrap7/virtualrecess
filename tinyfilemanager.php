c5bcca14eef6335e60b9f00c1a80c534b8ceebca
torrap7 <119638823+torrap7@users.noreply.github.com>
2023-07-26 14:59:06 -0400

    Update tinyfilemanager.php
    password

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -27,7 +27,7 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
+    'admin' => '$2y$10$DvGrSJ2OXoCabH6./AgiUeUIvnJ4Cpzj8GHv4YwxqexEwydQvjJpe',
     'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
 );
 
